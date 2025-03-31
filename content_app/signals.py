dd16311088f90d7d31c37eb47ae76f1b3d044e40
Kevin Mueller <kevin.mueller@tuta.com>
2025-03-31 18:05:37 +0200

    updated django-rq and rq requirements because of version incompatibilities

--- content_app/signals.py
+++ content_app/signals.py
@@ -1,5 +1,6 @@
 from .models import Video
 from django.dispatch import receiver
+from django.db import transaction
 from django.db.models.signals import post_save,post_delete
 import os
 from .tasks import convert_480p
@@ -16,7 +17,8 @@ def video_post_save(sender, instance, created, **kwargs):
     if created:
         print('New Video created')
         queue = django_rq.get_queue('default',autocommit=True)
-        queue.enqueue(convert_480p,instance.video_file.path)
+        transaction.on_commit(lambda: queue.enqueue(convert_480p, instance.video_file.path))
+        #queue.enqueue(convert_480p,instance.video_file.path)
     
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender,instance,**kwargs):
