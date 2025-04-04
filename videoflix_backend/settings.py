8b60d9f70324fcdc626a7855777f3010c6620d6d
Kevin Mueller <kevin.mueller@tuta.com>
2025-04-04 16:11:39 +0200

    added corsheaders and restframework to settings.py

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -32,9 +32,15 @@ INTERNAL_IPS=[
     '127.0.0.1'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
+
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +57,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL ='user_app.CustomUser'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
