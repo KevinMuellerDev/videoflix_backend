beffe5aaa8d5f36fbda1678f06880209737eb698
Fenrir <kevin.mueller@tuta.com>
2025-04-25 12:34:04 +0200

    added choices and trailer and screenshot to video model

--- content_app/models.py
+++ content_app/models.py
@@ -4,11 +4,33 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
+    GENRE_CHOICE = {
+        'Action': 'Action',
+        'Adventure': 'Adventure',
+        'Animation': 'Animation',
+        'Comedy': 'Comedy',
+        'Drama': 'Drama',
+        'Fantasy': 'Fantasy',
+        'Horror': 'Horror',
+        'Mystery': 'Mystery',
+        'Romance': 'Romance',
+        'Sci-Fi': 'Science Fiction',
+        'Thriller': 'Thriller',
+        'Documentary': 'Documentary',
+        'Musical': 'Musical',
+        'Western': 'Western',
+        'Crime': 'Crime',
+        'Biography': 'Biography',
+        'Sports': 'Sports',
+        'Historical': 'Historical',
+    }
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=80)
     description = models.TextField(max_length=500)
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
-    genre = models.CharField(max_length=80)
+    trailer = models.FileField(blank=True,null=True)
+    screenshot = models.FileField(blank=True,null=True)
+    genre = models.CharField(choices=GENRE_CHOICE,max_length=80)
 
     def __str__(self):
         return self.title
--- content_app/signals.py
+++ content_app/signals.py
@@ -9,18 +9,12 @@ import django_rq
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    print('Video wurde gespeichert')
     if created:
-        print('New Video created')
         queue = django_rq.get_queue('default', autocommit=True)
-
         transaction.on_commit(lambda: queue.enqueue(convert_to_hls, instance.video_file.path, instance.pk))
 
 
 def delete_folder_contents(folder_path):
-    """
-    Hilfsfunktion, die alle Dateien im angegebenen Ordner löscht.
-    """
     if os.path.isdir(folder_path):
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
@@ -28,6 +22,16 @@ def delete_folder_contents(folder_path):
                 os.remove(file_path)
 
         os.rmdir(folder_path)
+
+
+def delete_original_file(video_dir):
+    parent_dir=os.path.dirname(video_dir)
+    last_dir = os.path.basename(video_dir)
+    original_video_name = last_dir.replace('_hls','.mp4')
+    original_file = os.path.join(parent_dir,original_video_name)
+
+    os.remove(original_file)
+
 
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender, instance, **kwargs):
@@ -37,7 +41,6 @@ def video_post_delete(sender, instance, **kwargs):
         if os.path.isdir(video_dir):
             for folder_name in os.listdir(video_dir):
                 folder_path = os.path.join(video_dir, folder_name)
-                # Lösche nur die Ordner, die mit der Video-Verarbeitung zu tun haben (z.B. 240p, 480p, etc.)
                 if os.path.isdir(folder_path):
                     delete_folder_contents(folder_path)
 
@@ -45,11 +48,4 @@ def video_post_delete(sender, instance, **kwargs):
             os.remove(instance.video_file.path)
         os.rmdir(video_dir)
 
-        parent_dir=os.path.dirname(video_dir)
-        last_dir = os.path.basename(video_dir)
-        original_video_name = last_dir.replace('_hls','.mp4')
-        original_file = os.path.join(parent_dir,original_video_name)
-
-        os.remove(original_file)
-
-        print(f"Alle zugehörigen Dateien und Ordner für das Video '{instance.title}' wurden gelöscht.")
+        delete_original_file(video_dir=video_dir)
