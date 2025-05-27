a7963a9c2465d793d3304ca038b60a3d83c58f69
KevinMueller <kevin.mueller@tuta.com>
2025-05-27 14:57:59 +0200

    added dummy .env and package

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -203,13 +203,13 @@ DOMAIN = "localhost:3000"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('HOST')
-EMAIL_PORT = 465 
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True  
-EMAIL_HOST_USER = os.getenv('HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('HOST_USER')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv("EMAIL_PORT") 
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 DJOSER = {
     'SITE_NAME': 'Videoflix',
